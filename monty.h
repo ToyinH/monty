acd13751008bb6b74dde2084b3511036178143b3
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-24 15:23:03 +0000

    updated header

--- monty.h
+++ monty.h
@@ -51,6 +51,7 @@ void opcode_pop(stack_t **h, unsigned int line_number);
 void opcode_swap(stack_t **h, unsigned int line_number);
 void opcode_add(stack_t **h, unsigned int line_number);
 void opcode_nop(stack_t **h, unsigned int line_number);
+void opcode_sub(stack_t **h, unsigned int line_number);
 void free_stack(stack_t *head);
 int len(stack_t *h);
 
