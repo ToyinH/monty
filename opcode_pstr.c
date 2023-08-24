0453801a508a0b3beeb990b110583ae9d6d19089
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-24 18:09:52 +0000

    print string

---
+++ opcode_pstr.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * opcode_pstr - prints the string at the top of the stack
+ * @h: pointer to the head node
+ * @line_number: line number
+ */
+
+void opcode_pstr(stack_t **h, unsigned int line_number)
+{
+	stack_t *temp;
+	int num;
+
+	(void)line_number;
+	if (*h == NULL)
+	{
+		dprintf(1, "\n");
+	}
+	else
+	{
+		temp = *h;
+		while (temp != NULL)
+		{
+			num = temp->n;
+			if (num > 127 || num == 0)
+				break;
+			dprintf(1, "%c", (char)num);
+			temp = temp->next;
+		}
+		dprintf(1, "\n");
+	}
+}
