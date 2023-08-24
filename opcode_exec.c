8299be1c5440f1fb31caac4c0ced03f96ecd9b54
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-24 13:58:38 +0000

    function to execute opcode

--- opcode_exec.c
+++ opcode_exec.c
@@ -3,6 +3,7 @@
 void opcode_exec(char *opcode, stack_t **head, unsigned int line_number)
 {
     unsigned int i;
+    int valid_instruction_found;
 
     instruction_t instruct[] = {
         {"push", opcode_push},
@@ -12,13 +13,20 @@ void opcode_exec(char *opcode, stack_t **head, unsigned int line_number)
         {"swap", opcode_swap}
     };
 
+    valid_instruction_found = 0;
     for (i = 0; i < sizeof(instruct) / sizeof(instruct[0]); i++)
     {
         if (strcmp(instruct[i].opcode, opcode) == 0)
         {
             instruct[i].f(head, line_number);
+	    valid_instruction_found = 1;
             break;
         }
+	if (!valid_instruction_found)
+	{
+		dprintf(2, "L%d: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
 
     }
 }
