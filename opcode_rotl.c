77547570abdb7f7a07941228ae8672aec7232859
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-25 04:32:36 +0000

    func to take head to tail

---
+++ opcode_rotl.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * opcode_rotl - function that reverse linked list
+ * @h: pointer to header node
+ * @line_number: line number
+ */
+void opcode_rotl(stack_t **h, unsigned int line_number)
+{
+	stack_t *current, *current2, *ahead;
+
+	current = *h;
+	ahead = current->next;
+	current2 = *h;
+	(void)line_number;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current2->prev = current;
+	current2->next = NULL;
+	current->next = current2;
+	ahead->prev = NULL;
+	*h = ahead;
+}
