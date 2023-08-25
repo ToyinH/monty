3b93959f66c0693d912f58b68561687b1d81e87e
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-25 04:47:25 +0000

    func handl rotr

---
+++ opcode_rotr.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * opcode_rotr - function that takes last element to top
+ * @h: pointer to header node
+ * @line_number: line number
+ */
+void opcode_rotr(stack_t **h, unsigned int line_number)
+{
+	stack_t *current, *current2;
+
+	current = *h;
+	current2 = *h;
+	(void)line_number;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->prev->next = NULL;
+	current->next = current2;
+	current->prev = NULL;
+	current2->prev = current;
+	*h = current;
+}
