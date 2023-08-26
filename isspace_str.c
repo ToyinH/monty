9b43de830877bdfed0485646ee95c2b8dd41eb87
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-26 12:52:24 +0000

    function to handle spaces

---
+++ isspace_str.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * isspace_str - func to check for space and address them
+ * str: string to check
+ * Return: return 1 if a character is encountered.
+ */
+int isspace_str(char *str)
+{
+	int k = 0;
+
+	while(str[k] != '\0')
+	{
+		if (!isspace(str[k]))
+		{
+			return (1);
+		}
+		k++;
+	}
+	return (0);
+}
