d6d4c33f9dd1cba2b281e4af2006b99cd0b83aaf
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-26 13:56:34 +0000

    update to cater for empty node

--- opcode_rotl.c
+++ opcode_rotl.c
@@ -8,6 +8,8 @@ void opcode_rotl(stack_t **h, unsigned int line_number)
 {
 	stack_t *current, *current2, *ahead;
 
+	if (*h == NULL)
+		return;
 	current = *h;
 	ahead = current->next;
 	current2 = *h;
